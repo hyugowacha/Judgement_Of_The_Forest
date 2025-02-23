94134c5ce4ec58c26188aef85349bf604df58bb5
hyugowacha <uena0506@gmail.com>
2025-02-24 06:10:52 +0900

    [Feat] 캐릭터 이동 구현

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Animator animator;
+    Vector3 moveDir;
+    float TurnSpeed = 15.0f;
+    float moveSpeed = 4.0f;
+    Rigidbody rigid;
+
+    public GameObject PlayerWeapon;
+    public AnimatorController AttackAnimator;
+    public AnimatorController MoveAnimator;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (moveDir != Vector3.zero)
+        {
+            Quaternion PlayerTurn = Quaternion.LookRotation(moveDir);
+            rigid.MoveRotation(Quaternion.RotateTowards(rigid.rotation, PlayerTurn, TurnSpeed));
+            rigid.MovePosition(rigid.position + moveDir * Time.deltaTime * moveSpeed);
+
+        }
+    }
+
+
+    void OnMove(InputValue val)
+    {
+        PlayerWeapon.SetActive(false);
+        animator.runtimeAnimatorController = MoveAnimator;
+        Vector2 dir = val.Get<Vector2>();
+        moveDir = new Vector3(dir.x, 0, dir.y);
+
+        animator.SetFloat("isRun", dir.magnitude);
+    }
+
+    void OnAttack()
+    {
+        Debug.Log("АјАн");
+        animator.runtimeAnimatorController = AttackAnimator;
+        PlayerWeapon.SetActive(true);
+        animator.applyRootMotion = true;
     }
 }
